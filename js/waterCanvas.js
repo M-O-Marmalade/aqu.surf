5bc8f0db0db8a00f660b6f75f33fdee53adf2a41
M-O-Marmalade <xephyrpanda@yahoo.com>
2022-04-25 21:42:49 -0700

    setting up a background textured plane

--- js/waterCanvas.js
+++ js/waterCanvas.js
@@ -1,4 +1,6 @@
-let waterCanvas = document.createElement("canvas");
+const mobileBrowser = window.mobileCheck;
+
+const waterCanvas = document.createElement("canvas");
 waterCanvas.style.position = "fixed";
 waterCanvas.style.top = 0;
 waterCanvas.style.margin = 0;
@@ -7,8 +9,6 @@ waterCanvas.style.height = "100%";
 waterCanvas.style.overflow = "hidden";
 waterCanvas.style.zIndex = -10;
 document.body.appendChild(waterCanvas);
-
-const portrait = screen.width > screen.height ? false : true;
 
 const fps = 60;
 const msPerFrame = 1000/fps;
@@ -20,8 +20,10 @@ const pointerSize = 2;
 const totalSegments = 48000;
 let wX = window.innerWidth;
 let wY = window.innerHeight;
+let wAspectFloat = wX / wY;
 let wXYgcd = gcd(wX, wY);
 let aspectScale = aspectScaleSolve(wX/wXYgcd , wY/wXYgcd , totalSegments);
+console.log(aspectScale);
 const xSeg = Math.floor(wX/wXYgcd * aspectScale);
 const ySeg = Math.floor(wY/wXYgcd * aspectScale);
 const yPoi = ySeg + 1;
@@ -30,14 +32,17 @@ const xPoi = xSeg + 1;
 const prev = new Array(xPoi * yPoi).fill(0);
 const curr = new Array(xPoi * yPoi).fill(0);
 
+//Three.js settings
+THREE.Cache.enabled = true;
+
 //scene & camera
 const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0x0044ff );
 // const camera = new THREE.OrthographicCamera(-0.5, 0.5, 0.5, -0.5, 0.1, 1000);
-const camera = new THREE.PerspectiveCamera(53, wX / wY, 0.5, 1000);
+const camera = new THREE.PerspectiveCamera(26.5, wAspectFloat, 0.5, 1000);
 camera.position.x = 0;
 camera.position.y = 0;
-camera.position.z = 1;
+camera.position.z = 2.1;
 camera.lookAt(0,0,0);
 
 //renderer
@@ -46,15 +51,15 @@ const renderer = new THREE.WebGLRenderer({
 });
 // renderer.alpha = true;
 // renderer.setClearColor(0x000000, 0);
-if (!portrait) {  //if we're in landscape aspect, we'll assume desktop-grade performance
+if (!mobileBrowser) {   //full rendering resolution for desktops
     renderer.setPixelRatio( window.devicePixelRatio );
 }            
 renderer.setSize( wX, wY );
 
 // light
-const light = new THREE.DirectionalLight(0xffffff);
-light.position.set(2,2,10);
-//scene.add( light );
+const light = new THREE.DirectionalLight(0xf8f0f8, 1);
+light.position.set(-1,2,10);
+scene.add( light );
 
 // materials
 const refractiveMat = new THREE.MeshPhysicalMaterial({
@@ -63,7 +68,8 @@ const refractiveMat = new THREE.MeshPhysicalMaterial({
     roughness: 0,
     ior: 1.333,
     thickness: 10,
-    //reflectivity: 1,
+    specularIntensity: 1,
+    // reflectivity: 1,
 });
 
 const normalMat = new THREE.MeshNormalMaterial({
@@ -90,7 +96,6 @@ const waterGeometry = new THREE.BufferGeometry();
 const waterIndices = [];
 const waterPositions = [];
 const waterNormals = [];
-// const waterColors = [];
 
 let counter = 0;
 
@@ -101,11 +106,10 @@ for ( let i = 0; i < yPoi; i++ ) {
 
     for ( let j = 0; j < xPoi; j++ ) {
 
-        const x = ((j / xSeg) - 0.5) * wX / wY;    //set x position from 0 - 1
+        const x = ((j / xSeg) - 0.5) * wAspectFloat;    //set x position from 0 - 1
 
         waterPositions.push( x, y, 0 );
         waterNormals.push( 0, 0, -1 );
-        // waterColors.push( x,y,x/2 + y/2 );
 
         counter++;
     }
@@ -131,21 +135,19 @@ for ( let i = 0; i < ySeg; i++ ) {
 waterGeometry.setIndex( waterIndices );            
 const positionsA = new THREE.Float32BufferAttribute(waterPositions,3);
 const normalsA = new THREE.Float32BufferAttribute(waterNormals,3);
-// const colorsA = new THREE.Float32BufferAttribute(waterColors,3);
 waterGeometry.setAttribute( 'position', positionsA );
 waterGeometry.setAttribute( 'normal', normalsA );
-// waterGeometry.setAttribute( 'color', colorsA);
 const waterMesh = new THREE.Mesh( waterGeometry, refractiveMat );
 scene.add( waterMesh );
 
 //add torus to scene
-const torusGeometry = new THREE.TorusKnotGeometry(10, 2.3, 256, 64);
-const torusMesh = new THREE.Mesh( torusGeometry, normalMat);
-torusMesh.position.z = -0.5;
-torusMesh.scale.x = 0.06;
-torusMesh.scale.y = 0.032;
-torusMesh.scale.z = 0.04;
-scene.add(torusMesh);
+// const torusGeometry = new THREE.TorusKnotGeometry(10, 2.3, 256, 64);
+// const torusMesh = new THREE.Mesh( torusGeometry, normalMat);
+// torusMesh.position.z = -0.5;
+// torusMesh.scale.x = 0.06;
+// torusMesh.scale.y = 0.032;
+// torusMesh.scale.z = 0.04;
+// scene.add(torusMesh);
 
 //add bg plane to scene
 const bgPlaneGeometry = new THREE.BufferGeometry();
@@ -154,11 +156,23 @@ const bgPlaneIndices = [
     0, 2, 3
 ];
 const bgPlanePositions = [
-    -0.77, 0.77, -1.5,
-    0.77, 0.77, -1.5,
-    -0.77, -0.77, -1.5,
-    0.77, -0.77, -1.5,
+    -0.64 * wAspectFloat, 0.64, -1.5,
+    0.64 * wAspectFloat, 0.64, -1.5,
+    -0.64 * wAspectFloat, -0.64, -1.5,
+    0.64 * wAspectFloat, -0.64, -1.5,
 
+];
+const bgPlaneNormals = [
+    0,0,1,
+    0,0,1,
+    0,0,1,
+    0,0,1
+];
+const bgPlaneUVs = [
+    0,1,
+    1,1,
+    0,0,
+    1,0
 ];
 const bgPlaneColors = [
     0, 0.576, 1,
@@ -167,12 +181,43 @@ const bgPlaneColors = [
     0, 0.576, 1,
 ];
 
-//set water mesh data and add to scene
+//bgPlane material
+const bgPlaneTex = new THREE.TextureLoader().load('graphics/textures/texture.png');
+const bgPlaneMat = new THREE.MeshPhongMaterial({
+    map: bgPlaneTex
+    // vertexColors: true
+});
+
+//set bg mesh data and add to scene
 bgPlaneGeometry.setIndex( bgPlaneIndices );
 bgPlaneGeometry.setAttribute( 'position', new THREE.Float32BufferAttribute(bgPlanePositions,3) );
+bgPlaneGeometry.setAttribute( 'normal', new THREE.Float32BufferAttribute(bgPlaneNormals,3) );
+bgPlaneGeometry.setAttribute( 'uv', new THREE.Float32BufferAttribute(bgPlaneUVs,2) );
 bgPlaneGeometry.setAttribute( 'color', new THREE.Float32BufferAttribute(bgPlaneColors,3) );
-const bgPlaneMesh = new THREE.Mesh( bgPlaneGeometry, vertMat );
+const bgPlaneMesh = new THREE.Mesh( bgPlaneGeometry, bgPlaneMat );
 scene.add( bgPlaneMesh );
+
+
+
+// const loader = new THREE.FontLoader();
+
+// loader.load( 'fonts/monof55.ttf', function ( font ) {
+
+// 	const geometry = new TextGeometry( 'Hello three.js!', {
+// 		font: font,
+// 		size: 80,
+// 		height: 5,
+// 		curveSegments: 12,
+// 		bevelEnabled: true,
+// 		bevelThickness: 10,
+// 		bevelSize: 8,
+// 		bevelOffset: 0,
+// 		bevelSegments: 5
+// 	} );
+// } );
+
+
+
 
 document.body.addEventListener( 'mousemove', onMouseMove );
 document.body.addEventListener( 'touchmove', onTouchMove );
@@ -229,7 +274,7 @@ function rippleAnimate() {
 
     for (let i = 0; i < xPoi*yPoi; i++) {
         normalsA.setY(i,curr[i]);
-        normalsA.setX(i,curr[i]);
+        normalsA.setX(i,-curr[i]);
     }
 
     for (let i = 0; i < xPoi*yPoi; i++) {
@@ -283,6 +328,12 @@ function aspectScaleSolve(a,b,c) {
     c = -c;
     return (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
 }
+
+window.mobileCheck = function() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+};
 
 
 // <!-- <canvas id="waterCanvas" style="position: fixed; top: 0;"></canvas> -->
