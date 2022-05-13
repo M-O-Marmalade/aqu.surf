d8bad40e8b24ec20d2808bbd1144c08af76e1cf3
M-O-Marmalade <xephyrpanda@yahoo.com>
2022-05-13 16:35:31 -0700

    checkpoint, but things seem to be fully working!

--- flipCardsTest.js
+++ flipCardsTest.js
@@ -2,64 +2,88 @@
 //keep track of which element is currently flipped, and allow it to transition back to its original state when a button on the backside is clicked
 //only allow one element to be flipped at a time
 
+/*
+cardsMap struct:
+    cardID = [
+        isFlipped = bool,
+    ]
+*/
+
+const cardsMap = new Map();
 let isACardFlipped = false;
-let flippedCardOriginal;
 
 function closeFlipCard() {
-        console.log("close button clicked!");
-        if (!isACardFlipped) {return;}
 
-        let parent;
-        
+    isACardFlipped = false;
 
-        const elemRect = elem.getBoundingClientRect();  //store the element's bounding rectangle before any changes are made
+    //find .itemDiv ancestor of this button
+    let clone = this.parentElement;
+    while (1) {
+        if (clone.classList.contains("itemDiv")) {
+            break;
+        }
+        clone = clone.parentElement;
+    }
+    
+    console.log(clone.id + "'s close button clicked!");
+    console.log(clone.style.position);
 
-        const clone = elem.cloneNode(true); //create and add the clone to be flipped
-        document.body.after(clone);
+    const origCard = document.getElementById(clone.id.slice( 0, -5 ));
+    const container = origCard.parentElement;
 
-        elem.style.visibility = 'hidden';   //hide the original element
-        
-        clone.style.position = "fixed"; //set the clone's starting position/size to be transitioned from
-        clone.style.margin = '0px';
-        clone.style.top = elemRect.top + 'px';
-        clone.style.left = elemRect.left + 'px';
-        clone.style.width = elemRect.width + 'px';
-        clone.style.height = elemRect.height + 'px';
+    const cloneTop = clone.getBoundingClientRect().top;
+    const cloneLeft = clone.getBoundingClientRect().left;
+    const origTop = origCard.getBoundingClientRect().top;
+    const origLeft = origCard.getBoundingClientRect().left;
+    const origWidth = origCard.getBoundingClientRect().width;
+    const origHeight = origCard.getBoundingClientRect().height;
+    const containerTop = origCard.parentElement.getBoundingClientRect().top;
+    const containerLeft = origCard.parentElement.getBoundingClientRect().left;
 
+    clone.style.top = '0px';
+    clone.style.left = '0px';
+    clone.style.margin = (cloneTop - containerTop) + 'px 0px 0px ' + (cloneLeft - containerLeft) + 'px';
+    clone.style.position = 'absolute';
+    container.insertBefore(clone, container.firstChild);
 
-        requestAnimationFrame(function() {  //set the clone's ending position/size to be transitioned to on the next frame, so it will actually transition
-            requestAnimationFrame(function() {
-                clone.style.width = '90vw';
-                clone.style.height = '90vh';
-                clone.style.top = '5vh'
-                clone.style.left = '5vw';
-                clone.classList.add("clicked");            
+    requestAnimationFrame(function() {  //set the clone's ending position/size to be transitioned to on the next frame, so it will actually transition
+        requestAnimationFrame(function() {
+            clone.style.width = origWidth + 'px';
+            clone.style.height = origHeight + 'px';
+            clone.style.margin = clone.style.margin = (origTop - containerTop) + 'px 0px 0px ' + (origLeft - containerLeft) + 'px';
+            clone.style.transform = 'rotateY(0deg)';
+            clone.addEventListener('transitionend', function() {
+                origCard.style.visibility = 'visible';
+                clone.remove();
             });
         });
+    });
 };
 
 document.querySelectorAll('.itemDiv').forEach(item => {
-    item.addEventListener('click', function(e) {
-        console.log("card clicked!");
-        if (isACardFlipped) {return;}
+    item.addEventListener('click', function(e) {        
+        console.log(this.id + " clicked!");
+        
+        if (isACardFlipped) {return;}   //exit if a card is already flipped
         isACardFlipped = true;  //record that we have a card flipped
 
-        const elem = e.currentTarget;
-        flippedCardOriginal = elem; //store a reference to the original card
+        const origRect = this.getBoundingClientRect();  //store the element's bounding rectangle before any changes are made
 
-        const elemRect = elem.getBoundingClientRect();  //store the element's bounding rectangle before any changes are made
+        const clone = this.cloneNode(true); //create and add the clone to be flipped
+        clone.id = clone.id + "clone";
+        clone.querySelector('.closeCardButton').addEventListener('click', closeFlipCard);
 
-        const clone = elem.cloneNode(true); //create and add the clone to be flipped
-        document.body.after(clone);
+        // document.body.after(clone);
+        document.body.insertBefore(clone, document.body.firstChild);
 
-        elem.style.visibility = 'hidden';   //hide the original element
+        this.style.visibility = 'hidden';   //hide the original element
         
-        clone.style.position = "fixed"; //set the clone's starting position/size to be transitioned from
+        clone.style.position = 'fixed'; //set the clone's starting position/size to be transitioned from
         clone.style.margin = '0px';
-        clone.style.top = elemRect.top + 'px';
-        clone.style.left = elemRect.left + 'px';
-        clone.style.width = elemRect.width + 'px';
-        clone.style.height = elemRect.height + 'px';
+        clone.style.top = origRect.top + 'px';
+        clone.style.left = origRect.left + 'px';
+        clone.style.width = origRect.width + 'px';
+        clone.style.height = origRect.height + 'px';
 
 
         requestAnimationFrame(function() {  //set the clone's ending position/size to be transitioned to on the next frame, so it will actually transition
@@ -68,7 +92,11 @@ document.querySelectorAll('.itemDiv').forEach(item => {
                 clone.style.height = '90vh';
                 clone.style.top = '5vh'
                 clone.style.left = '5vw';
-                clone.classList.add("clicked");            
+                clone.style.transform = 'rotateY(180deg)';
+                clone.style.zIndex = '10';
+                clone.querySelector('.frontDiv').style.position = 'absolute';
+                clone.querySelector('.backDiv').style.position = 'static';
+                clone.style.position = 'fixed';
             });
         });
 
