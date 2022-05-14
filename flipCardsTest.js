b4c3645f5050139914f16b8f3148b95fc7f96d70
M-O-Marmalade <xephyrpanda@yahoo.com>
2022-05-13 19:41:09 -0700

    decoupled some code a bit. flipCardTests.html appears to be fully functional!

--- flipCardsTest.js
+++ flipCardsTest.js
@@ -1,25 +1,64 @@
-//TODO
-//keep track of which element is currently flipped, and allow it to transition back to its original state when a button on the backside is clicked
-//only allow one element to be flipped at a time
-
-/*
-cardsMap struct:
-    cardID = [
-        isFlipped = bool,
-    ]
-*/
-
-const cardsMap = new Map();
 let isACardFlipped = false;
+let flipCardUniqueIDCounter = 0;
+
+const cardClass = "itemDiv";
+const closeButtonClass = "closeCardButton";
+const frontClass = "frontDiv";
+const backClass = "backDiv";
+
+
+document.querySelectorAll('.' + cardClass).forEach(item => {
+    item.addEventListener('click', function(e) {        
+        if (!this.id) {this.id = "flipCardUniqueID" + flipCardUniqueIDCounter++;}
+        console.log(this.id + " clicked!");
+        
+        if (isACardFlipped) {return;}   //exit if a card is already flipped
+        isACardFlipped = true;  //record that we have a card flipped
+
+        const origRect = this.getBoundingClientRect();  //store the element's bounding rectangle before any changes are made
+
+        const clone = this.cloneNode(true); //create and add the clone to be flipped
+        clone.id = clone.id + "clone";
+        clone.querySelector('.' + closeButtonClass).addEventListener('click', closeFlipCard);
+
+        document.body.insertBefore(clone, document.body.firstChild);
+
+        this.style.visibility = 'hidden';   //hide the original element
+        
+        clone.style.position = 'fixed'; //set the clone's starting position/size to be transitioned from
+        clone.style.margin = '0px';
+        clone.style.top = origRect.top + 'px';
+        clone.style.left = origRect.left + 'px';
+        clone.style.width = origRect.width + 'px';
+        clone.style.height = origRect.height + 'px';
+
+
+        requestAnimationFrame(function() {  //set the clone's ending position/size to be transitioned to on the next frame, so it will actually transition
+            requestAnimationFrame(function() {
+                clone.style.width = '90vw';
+                clone.style.height = '80vh';
+                clone.style.top = '10vh'
+                clone.style.left = '5vw';
+                clone.style.transform = 'rotateY(180deg)';
+                clone.style.zIndex = '10';
+                clone.querySelector('.' + frontClass).style.position = 'absolute';
+                clone.querySelector('.' + backClass).style.position = 'static';
+                clone.style.position = 'fixed';
+            });
+        });
+
+    });
+});
 
 function closeFlipCard() {
 
+    if (!isACardFlipped) {return;}
     isACardFlipped = false;
 
-    //find .itemDiv ancestor of this button
+    //find .cardClass ancestor of this button
     let clone = this.parentElement;
     while (1) {
-        if (clone.classList.contains("itemDiv")) {
+        if (clone.classList.contains(cardClass)) {
             break;
         }
         clone = clone.parentElement;
@@ -59,46 +98,3 @@ function closeFlipCard() {
         });
     });
 };
-
-document.querySelectorAll('.itemDiv').forEach(item => {
-    item.addEventListener('click', function(e) {        
-        console.log(this.id + " clicked!");
-        
-        if (isACardFlipped) {return;}   //exit if a card is already flipped
-        isACardFlipped = true;  //record that we have a card flipped
-
-        const origRect = this.getBoundingClientRect();  //store the element's bounding rectangle before any changes are made
-
-        const clone = this.cloneNode(true); //create and add the clone to be flipped
-        clone.id = clone.id + "clone";
-        clone.querySelector('.closeCardButton').addEventListener('click', closeFlipCard);
-
-        // document.body.after(clone);
-        document.body.insertBefore(clone, document.body.firstChild);
-
-        this.style.visibility = 'hidden';   //hide the original element
-        
-        clone.style.position = 'fixed'; //set the clone's starting position/size to be transitioned from
-        clone.style.margin = '0px';
-        clone.style.top = origRect.top + 'px';
-        clone.style.left = origRect.left + 'px';
-        clone.style.width = origRect.width + 'px';
-        clone.style.height = origRect.height + 'px';
-
-
-        requestAnimationFrame(function() {  //set the clone's ending position/size to be transitioned to on the next frame, so it will actually transition
-            requestAnimationFrame(function() {
-                clone.style.width = '90vw';
-                clone.style.height = '90vh';
-                clone.style.top = '5vh'
-                clone.style.left = '5vw';
-                clone.style.transform = 'rotateY(180deg)';
-                clone.style.zIndex = '10';
-                clone.querySelector('.frontDiv').style.position = 'absolute';
-                clone.querySelector('.backDiv').style.position = 'static';
-                clone.style.position = 'fixed';
-            });
-        });
-
-    });
-});
