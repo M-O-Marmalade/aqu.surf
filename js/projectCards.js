f0498900f9d72ee4a89f2ab4e3c35cf8c91d371a
M-O-Marmalade <xephyrpanda@yahoo.com>
2022-06-10 19:25:18 -0700

    almost done with writeups!

--- js/projectCards.js
+++ js/projectCards.js
@@ -7,72 +7,67 @@ const frontClass = "projectFront";
 const backClass = "projectBack";
 
 
-document.querySelectorAll('.' + cardClass).forEach(item => {
-    item.addEventListener('click', function(e) {        
-        if (!this.id) {this.id = "flipCardUniqueID" + flipCardUniqueIDCounter++;}
-        console.log(this.id + " clicked!");
-        
-        if (isACardFlipped) {return;}   //exit if a card is already flipped
-        isACardFlipped = true;  //record that we have a card flipped
+function openFlipCard(card) {
+    if (!card.id) {card.id = "flipCardUniqueID" + flipCardUniqueIDCounter++;}
+    console.log(card.id + " clicked!");
+    
+    if (isACardFlipped) {return;}   //exit if a card is already flipped
+    isACardFlipped = true;  //record that we have a card flipped
 
-        const origRect = this.getBoundingClientRect();  //store the element's bounding rectangle before any changes are made
+    const origRect = card.getBoundingClientRect();  //store the element's bounding rectangle before any changes are made
 
-        const clone = this.cloneNode(true); //create and add the clone to be flipped
-        clone.id = clone.id + "clone";
-        clone.querySelector('.' + closeButtonClass).addEventListener('click', closeFlipCard);
+    const clone = card.cloneNode(true); //create and add the clone to be flipped
+    clone.id = clone.id + "clone";
+    clone.querySelector('.' + closeButtonClass).addEventListener('click', function() {
+        closeFlipCard(clone);
+    });
 
-        document.body.lastChild.after(clone);
+    document.body.lastChild.after(clone);
 
-        this.style.visibility = 'hidden';   //hide the original element
-        
-        clone.style.transform = 'translateZ(999px)';
-        clone.style.transition = '1s, visibility 0s';
-        clone.style.position = 'fixed'; //set the clone's starting position/size to be transitioned from
-        clone.style.margin = '0px';
-        clone.style.top = origRect.top + 'px';
-        clone.style.left = origRect.left + 'px';
-        clone.style.width = origRect.width + 'px';
-        clone.style.height = origRect.height + 'px';
+    card.style.visibility = 'hidden';   //hide the original element
+    
+    clone.style.transform = 'translateZ(999px)';
+    clone.style.transition = '1s, visibility 0s';
+    clone.style.position = 'fixed'; //set the clone's starting position/size to be transitioned from
+    clone.style.margin = '0px';
+    clone.style.top = origRect.top + 'px';
+    clone.style.left = origRect.left + 'px';
+    clone.style.width = origRect.width + 'px';
+    clone.style.height = origRect.height + 'px';
 
 
-        requestAnimationFrame(function() {  //set the clone's ending position/size to be transitioned to on the next frame, so it will actually transition
-            requestAnimationFrame(function() {
-                clone.style.top = '5vh'
-                clone.style.left = '3vw';
-                clone.style.width = '94vw';
-                clone.style.height = '90vh';
-                clone.style.transform = 'rotateY(180deg) translateZ(-43px)';
-                clone.style.zIndex = 10;
-                clone.querySelector('.' + frontClass).style.position = 'absolute';
-                clone.querySelector('.' + backClass).style.position = 'static';
-            });
+    requestAnimationFrame(function() {  //set the clone's ending position/size to be transitioned to on the next frame, so it will actually transition
+        requestAnimationFrame(function() {
+            clone.style.top = '5vh'
+            clone.style.left = '3vw';
+            clone.style.width = '94vw';
+            clone.style.height = '90vh';
+            clone.style.transform = 'rotateY(180deg) translateZ(-43px)';
+            clone.style.zIndex = 10;
+            clone.querySelector('.' + frontClass).style.position = 'absolute';
+            clone.querySelector('.' + backClass).style.position = 'static';
         });
+    });
+}
 
+document.querySelectorAll('.' + cardClass).forEach(item => {
+    item.addEventListener('click', function() {
+        openFlipCard(item);
     });
 });
 
-function closeFlipCard() {
-
+function closeFlipCard(card) {
     if (!isACardFlipped) {return;}
     isACardFlipped = false;
-
-    //find .cardClass ancestor of this button
-    let clone = this.parentElement;
-    while (1) {
-        if (clone.classList.contains(cardClass)) {
-            break;
-        }
-        clone = clone.parentElement;
-    }
     
-    console.log(clone.id + "'s close button clicked!");
-    console.log(clone.style.position);
+    console.log(card.id + "'s close button clicked!");
+    console.log(card.style.position);
 
-    const origCard = document.getElementById(clone.id.slice( 0, -5 ));
+    const origCard = document.getElementById(card.id.slice( 0, -5 ));
     const container = origCard.parentElement;
 
-    const cloneTop = clone.getBoundingClientRect().top;
-    const cloneLeft = clone.getBoundingClientRect().left;
+    const cardTop = card.getBoundingClientRect().top;
+    const cardLeft = card.getBoundingClientRect().left;
     const origTop = origCard.getBoundingClientRect().top;
     const origLeft = origCard.getBoundingClientRect().left;
     const origWidth = origCard.getBoundingClientRect().width;
@@ -80,25 +75,31 @@ function closeFlipCard() {
     const containerTop = origCard.parentElement.getBoundingClientRect().top;
     const containerLeft = origCard.parentElement.getBoundingClientRect().left;
 
-    clone.style.top = '0px';
-    clone.style.left = '0px';
-    clone.style.margin = (cloneTop - containerTop) + 'px 0px 0px ' + (cloneLeft - containerLeft) + 'px';
-    clone.style.position = 'absolute';
-    container.insertBefore(clone, container.firstChild);
+    card.style.top = '0px';
+    card.style.left = '0px';
+    card.style.margin = (cardTop - containerTop) + 'px 0px 0px ' + (cardLeft - containerLeft) + 'px';
+    card.style.position = 'absolute';
+    container.insertBefore(card, container.firstChild);
 
-    requestAnimationFrame(function() {  //set the clone's ending position/size to be transitioned to on the next frame, so it will actually transition
+    requestAnimationFrame(function() {  //set the card's ending position/size to be transitioned to on the next frame, so it will actually transition
         requestAnimationFrame(function() {
-            clone.style.width = origWidth + 'px';
-            clone.style.height = origHeight + 'px';
-            clone.style.margin = clone.style.margin = (origTop - containerTop) + 'px 0px 0px ' + (origLeft - containerLeft) + 'px';
-            clone.style.transform = 'rotateY(0deg)';
-            clone.addEventListener('transitionend', function() {
+            card.style.width = origWidth + 'px';
+            card.style.height = origHeight + 'px';
+            card.style.margin = card.style.margin = (origTop - containerTop) + 'px 0px 0px ' + (origLeft - containerLeft) + 'px';
+            card.style.transform = 'rotateY(0deg)';
+            card.addEventListener('transitionend', function() {
                 origCard.style.visibility = 'visible';
-                clone.remove();
+                card.remove();
             });
         });
     });
 };
+
+function switchCard(thisEl, closeID, openID) {
+    thisEl.style.transition = '0s';
+    closeFlipCard(document.getElementById(closeID + 'clone'));
+    setTimeout(function(){openFlipCard(document.getElementById(openID))},347);
+}
 
 
 
