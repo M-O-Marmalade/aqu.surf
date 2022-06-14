368d1ad7ba61e46f50052dfa0d427b1251067c96
M-O-Marmalade <xephyrpanda@yahoo.com>
2022-06-14 15:50:15 -0700

    added scroll toggle button on touch devices, and camera now moves with scrolling

--- js/waterCanvas.js
+++ js/waterCanvas.js
@@ -9,7 +9,7 @@ const zAxis = new THREE.Vector3(0, 0, 1);
 // exampleVector.applyAxisAngle(xAxis, 1);
 // console.log(exampleVector.x + ' : ' + exampleVector.y + ' : ' + exampleVector.z);
 
-const mobileBrowser = window.mobileCheck;
+const portrait = screen.height > screen.width ? true : false;
 
 const waterCanvas = document.createElement("canvas");
 waterCanvas.style.position = "fixed";
@@ -53,7 +53,7 @@ const gltfLoader = new GLTFLoader();
 const renderer = new THREE.WebGLRenderer({
     canvas: waterCanvas
 });
-if (!mobileBrowser) {   //full rendering resolution for desktops only
+if (!portrait) {
     renderer.setPixelRatio(window.devicePixelRatio);
 }
 renderer.setSize(wX, wY);
@@ -169,22 +169,23 @@ torusMesh.scale.z = 0.04;
 scene.add(torusMesh);
 
 
-// //add name plane to scene
-// const nameGeometry = new THREE.PlaneGeometry(26, 13);
-// const nameTex = textureLoader.load('graphics/textures/texture4.png');
-// const nameMat = new THREE.MeshBasicMaterial({map: nameTex});
-// const nameMesh = new THREE.Mesh( nameGeometry, nameMat );
-// nameMesh.position.z = -5;
-// if (wAspectFloat < 1) {
-//     nameMesh.scale.set(wAspectFloat,wAspectFloat,wAspectFloat);
-//     // nameMesh.position.y = .5 * (1 - wAspectFloat);
-// }
-// scene.add( nameMesh );
+//add name plane to scene
+const nameGeometry = new THREE.PlaneGeometry(26, 13);
+const nameTex = textureLoader.load('graphics/textures/texture4.png');
+const nameMat = new THREE.MeshBasicMaterial({map: nameTex});
+const nameMesh = new THREE.Mesh( nameGeometry, nameMat );
+nameMesh.position.z = -5;
+nameMesh.position.y = -1;
+if (wAspectFloat < 1) {
+    nameMesh.scale.set(0.02,0.02,0.02);
+    // nameMesh.position.y = .5 * (1 - wAspectFloat);
+}
+scene.add( nameMesh );
 
 
 //add name model to scene
 gltfLoader.load("graphics/models/james-graham.glb", function (gltf) {
-    gltf.scene.position.z = -2.5;
+    gltf.scene.position.z = -0.25;
     if (wAspectFloat < 1) {
         gltf.scene.scale.set(wAspectFloat, wAspectFloat, wAspectFloat);
         // nameMesh.position.y = .5 * (1 - wAspectFloat);
@@ -273,6 +274,13 @@ function rippleAnimate() {
     normalsA.needsUpdate = true;
 }
 
+function scrollAnimate() {
+    const scrolledPages = window.scrollY / window.innerHeight;
+    const offsetAmount = -0.9;
+    camera.position.y = scrolledPages * offsetAmount;
+    waterMesh.position.y = scrolledPages * offsetAmount;
+}
+
 function animate(sysTime) {
     const cumulativeTime = extraFrameTime + sysTime - prevFrameTime;
     if (cumulativeTime >= msPerFrame) {
@@ -281,6 +289,7 @@ function animate(sysTime) {
         // console.log("prev: " + prevFrameTime);
         // console.log("extra: " + extraFrameTime);
         rippleAnimate();
+        scrollAnimate();
         renderer.render(scene, camera);
     }
 
@@ -311,9 +320,3 @@ function aspectScaleSolve(a, b, c) {
     c = -c;
     return (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
 }
-
-window.mobileCheck = function () {
-    let check = false;
-    (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
-    return check;
-};
